e651d01c7d83d1348ff47f920c42fdc8222f54c6
清家良太 <seike@coopal.jp>
2014-09-22 09:21:55 +0900

    no message

--- src/com/example/hateburssreader/MainActivity.java
+++ src/com/example/hateburssreader/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.hateburssreader;
 
+import java.io.InputStream;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 
 import org.xmlpull.v1.XmlPullParser;
 
@@ -11,6 +13,9 @@ import android.util.Log;
 import android.util.Xml;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -19,22 +24,70 @@ public class MainActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		HttpURLConnection con = null;
+		InputStream in = null;
+		String body = "";
 		try {
-			XmlPullParser xmlPullParser = Xml.newPullParser();
+			// URLの作成
 			URL url = new URL("http://b.hatena.ne.jp/hotentry.rss");
-			URLConnection connection = url.openConnection();
-			xmlPullParser.setInput(connection.getInputStream(), "UTF-8");
 
-			int eventType;
-			while ((eventType = xmlPullParser.next()) != XmlPullParser.END_DOCUMENT) {
-				if ("title".equals(xmlPullParser.getName())) {
-					Log.d("xml_title", xmlPullParser.getName());
-					Log.d("xml_description", xmlPullParser.getText());
-				}
+			// 接続用HttpURLConnectionオブジェクト作成
+			con = (HttpURLConnection) url.openConnection();
+			// リクエストメソッドの設定
+			con.setRequestMethod("GET");
+			// 接続
+			con.connect();
+			// HTMLソースを読み出す
+			in = con.getInputStream();
+			byte[] line = new byte[1024];
+			int size;
+			while (true) {
+				size = in.read(line);
+				if (size <= 0)
+					break;
+				body += new String(line);
 			}
 		} catch (Exception e) {
 
+		} finally {
+			con.disconnect();
 		}
+
+		TextView text = (TextView) findViewById(R.id.textView);
+//		text.setText(body);
+
+		String title = "";
+		String link = "";
+		String body2 = "";
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+	            this, android.R.layout.simple_list_item_1);
+		XmlPullParser xmlPullParser = Xml.newPullParser();
+		try {
+			xmlPullParser.setInput(new StringReader(body));
+			int eventType;
+			while ((eventType = xmlPullParser.next()) != XmlPullParser.END_DOCUMENT) {
+				if (eventType == XmlPullParser.START_TAG
+						&& "title".equals(xmlPullParser.getName())) {
+					title = xmlPullParser.nextText();
+				}
+				if (eventType == XmlPullParser.START_TAG
+						&& "link".equals(xmlPullParser.getName())) {
+					link = xmlPullParser.nextText();
+				}
+				if (title != "" && link != "") {
+//					body2 += ("title:" + title + ", link:" + link + "¥n");
+					adapter.add(title);
+					title = "";
+					link = "";
+				}
+				Log.d("body2:", body2);
+			}
+		} catch (Exception e) {
+			Log.d("error:", e.getMessage());
+		}
+//		text.setText(body2);
+		ListView listView = (ListView) findViewById(R.id.listView1);
+		listView.setAdapter(adapter);
 	}
 
 	@Override
